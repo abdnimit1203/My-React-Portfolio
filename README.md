f409c5eb4c194deddb69c5cc4d64618a97242836
abdnimit1203 <abdullah.ibneali152pro@gmail.com>
2023-12-11 03:51:47 +0600

    Project built and necessary packages installed + Homepage created

---
+++ README.md
@@ -0,0 +1,7 @@
+# Welcome to my Portfolio website!
+
+## Hello ! My name is ABDULLAH IBNE ALI
+### Front-End Developer 
+### With MERN Stack Enthusiast 
+
+This is my main portfolio website repository. I have built this project using ReactJS in the frontend. 
---
+++ src/pages/Homepage.jsx
@@ -0,0 +1,11 @@
+
+const Homepage = () => {
+    return (
+        <div>
+            <h1 className="text-2xl">Hello, I am Abdullah</h1>
+            <p className="transition duration-300  hover:scale-95">FrontEnd Developer</p>
+        </div>
+    );
+};
+
+export default Homepage;
